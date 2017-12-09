f7de5b457e5d753b12602238b943ee0466cbea36
tal shachar <tal.shachar@buzzfeed.com>
2017-12-09 22:15:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,3 +58,24 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count (board)
+  counter = 0
+  board.each do |turn|
+  if turn == "X" || turn == "O"
+     counter += 1
+  end
+end
+counter
+end
+
+def current_player(board)
+
+  if turn_count(board) == 0
+    return "X"
+  elsif turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
