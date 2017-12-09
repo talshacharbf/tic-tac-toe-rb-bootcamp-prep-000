867189485c8d5bcc0715d3556466485e6e4d9bab
tal shachar <tal.shachar@buzzfeed.com>
2017-12-09 22:12:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,60 @@
 
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def display_board(board)
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts "-----------"
+ puts " #{board[3]} | #{board[4]} | #{board[5]} "
+ puts "-----------"
+ puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  converted_input = "#{user_input}".to_i - 1
+end
+
+def move(board, converted_input, character)
+  board[converted_input] = character
+end
+
+def position_taken?(board, index)
+  position = nil
+    if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    position = false
+    else
+    position = true
+    end
+    position
+
+end
+
+def valid_move?(board, index)
+move = nil
+  if index.between?(0,8) && !position_taken?(board, index)
+    move = true
+  else
+    move = false
+  end
+  move
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  converted_input = input_to_index(user_input)
+  if valid_move?(board, converted_input) == true
+  move(board, converted_input, character)
+  display_board(board)
+  else
+    turn(board)
+  end
+end
